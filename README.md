2dd45e9d7578c9259f7d964c5b128a41810ca9fa
Arnaud Pinoit <arnaud.pinoit@gmail.com>
2021-01-01 16:05:30 +0100

    day 14 version (regular)

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Reading list project of the 30 days of Python:
 https://blog.tecladocode.com/30-days-of-python/
 
-Started with day 12 project
+Contains multiple versions
 
  - Day 12: https://blog.tecladocode.com/python-30-day-12-project/
+ - Day 14 (regular): https://blog.tecladocode.com/python-30-day-14-project-regular/
--- reading_list.py
+++ reading_list.py
@@ -1,37 +1,96 @@
 #
-# https://blog.tecladocode.com/python-30-day-12-project/
+# https://blog.tecladocode.com/python-30-day-14-project-regular/
 #
-# Day 12 project: Reading list
+# Day 14 Project: Reading List
 #
 
-reading_list = []
-menu_prompt = "What do you want to do? \n 1. Add a book \n 2. Display your reading list\n 3. Quit\n"
+menu_prompt = """What do you want to do?
+1. Add a book
+2. Search a book
+3. Display your reading list
+4. Quit
+"""
+
+books = "books.csv"
+
 
 # Add a book: book title, author's name, year of publication
 def add_book():
     title = input("Title: ").strip().title()
     author = input("Author: ").strip().title()
     year = input("Year of publication: ").strip()
-    reading_list.append({
-        "title": title,
-        "author": author,
-        "year": year
-    })
 
-# Display all the books in the reading list
-def display_books():
-    for book in reading_list:
-        print(f"{book['title']}, {book['author']} ({book['year']})")
+    with open(books, "a") as book_file:
+        book_file.write(f"{title},{author},{year}\n")
+
+
+def retrieve_books():
+    book_shelf = []
+
+    with open(books, "r") as book_file:
+        books_data = book_file.readlines()
+
+    for row in books_data:
+        title, author, year = row.strip().split(",")
+
+        book_shelf.append({
+            "title": title,
+            "author": author,
+            "year": year
+        })
+    
+    return book_shelf
+
+
+# Search a book by title
+def search_book():
+    book_shelf = retrieve_books()
+    matching_books = []
+
+    title = input("Enter the book title: ").strip().lower()
+    for book in book_shelf:
+        if(title == book["title"].lower()):
+            matching_books.append(book)
+
+    return matching_books
+
+
+# Display the books passed as an argument
+def display_books(books):
+    for book in books:
+        print("- " + ", ".join(book.values()))
+
 
 # Select add or display from a text menu, multiple operations allowed
-while True:
-    action = input(menu_prompt)
-    if action == '1':
-        add_book()
-    elif action == '2':
-        if reading_list:
-            display_books()
+def main():
+    action = input(menu_prompt).strip().lower()
+    while True: 
+        if action == '1':
+            add_book()
+
+        elif action == '2':
+            matching_books = search_book()
+            if matching_books:
+                display_books(matching_books)
+            else:
+                print("This book is not part of your reading list")
+
+        elif action == '3':
+            books = retrieve_books()
+            if books:
+                display_books(books)
+            else:
+                print("Your reading list is empty")
+
+        elif action == '4':
+            break
+
         else:
-            print("Your reading list is empty.")
-    else:
-        break
+            print("Invalid option. Please try again.")
+
+        action = input("Enter your next choice: ")
+
+
+# Run the main program
+if __name__ == "__main__":
+    main()
